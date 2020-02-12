8a5f3119dc8ff6a4ba1cb75bc514d905e40867cd
Justin Ugaitafa <jugaitafa@nimoy.as.arizona.edu>
2020-02-11 20:05:04 -0700

    update

--- Homeworks/HW2/ReadFile.py
+++ Homeworks/HW2/ReadFile.py
@@ -1,22 +1,26 @@
 import numpy as np
 import astropy.units as u
 
-def Read(ReadFile): #defining the Read file as a function
+def Read(File): #defining the Read file as a function
 
-    file = open(ReadFile,'r')
+    file = open(File,'r')
 
     line1 = file.readline()
     label, value = line1.split()
     time = float(value)*u.Myr
+
     line2 = file.readline()
     label, value = line2.split()
     particle_num = float(value)
 
     file.close()
     
-    data = np.genfromtxt(x,dtype=None,names=True,skip_header=3)
+    data = np.genfromtxt(File,dtype=None,names=True,skip_header=3)
 
     return time,particle_num,data
   
 time,particle_num,data = Read('MW_000.txt')
-print(data['m'][1])
+#time,particle_num,data = Read('M31_000.txt')
+#time,particle_num,data = Read('M33_000.txt')
+
+#print(data['m'][1])
